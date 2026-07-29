bb2cda9ef3fc02a3f63b203fd3242bb70f2d7375
eymenasafsakalli0540-beep <eymenasafsakalli0540@gmail.com>
2026-07-29 16:13:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1036,7 +1036,6 @@ world_countries = {
     "bulgaristan": {"b": "Sofya", "k": "Avrupa", "lat": 42.70, "lon": 23.32, "bilgi": "Balkanlar'da yer alan, Türkiye'nin komşusu olan bir ülkedir."},
     "sirbistan": {"b": "Belgrad", "k": "Avrupa", "lat": 44.79, "lon": 20.45, "bilgi": "Balkanlar'ın merkezinde yer alan, denize kıyısı olmayan bir ülkedir."},
     "kibris": {"b": "Lefkoşa", "k": "Asya/Avrupa", "lat": 35.19, "lon": 33.38, "bilgi": "Akdeniz'in doğusunda yer alan bir ada ülkesidir."},
-    "birleşik krallık:{"b": "londra", "k": "avrupa", "lat": 51.50, "lon": -00.12, "bilgi": "Büyük Britanya adasında yer alan, küresel finans ve kültür merkezi olan bir krallıktır.
     "slovakya": {"b": "Bratislava", "k": "Avrupa", "lat": 48.14, "lon": 17.10, "bilgi": "Orta Avrupa'da yer alan, kaleleri ve kişi başına düşen otomobil üretimiyle ünlü bir ülkedir."},
     "hirvatistan": {"b": "Zagreb", "k": "Avrupa", "lat": 45.81, "lon": 15.97, "bilgi": "Adriyatik Denizi kıyısında uzun bir sahili ve binlerce adası bulunan bir Balkan ülkesidir."},
     "bosna_hersek": {"b": "Saraybosna", "k": "Avrupa", "lat": 43.85, "lon": 18.41, "bilgi": "Balkanlar'da yer alan, çok kültürlü yapısı ve tarihi Mostar Köprüsü ile tanınan bir ülkedir."},
